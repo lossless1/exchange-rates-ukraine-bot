6fea38c1a6f9b62da4f5e1317d12a4a3f79c652b
Volodymyr Saakian <vsaakian@lohika.com>
2020-03-22 20:48:53 +0200

    Update env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,28 +3,29 @@ module.exports = {
     {
       name: 'Exchange bot',
       script: './src/index.js',
-      instances: 1,
+      instances: 2,
       autorestart: true,
       watch: true,
-      max_memory_restart: '200M',
+      exec_mode: 'cluster',
+      max_memory_restart: '1000M',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
-      }
-    }
+        NODE_ENV: 'production',
+      },
+    },
   ],
 
   deploy: {
     production: {
-      user: 'node',
-      host: '212.83.163.1',
+      user: 'ubuntu',
+      host: 'ec2-52-15-67-42.us-east-2.compute.amazonaws.com',
       ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: '/var/www/production',
+      repo: 'git@github.com:lossless1/exchange-rates-ukraine-bot.git',
+      path: '/home/ubuntu/exchange-rates-ukraine-bot',
       'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
 };
